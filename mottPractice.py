28d63ea044ba6c54d330bf5bb5dc07010a91158b
AlwaysHigher <31978167+AlwaysHigher@users.noreply.github.com>
2019-10-05 17:02:47 +1300

    Update mottPractice.py

--- mottPractice.py
+++ mottPractice.py
@@ -3,16 +3,23 @@ from datetime import datetime
 
 df = pd.read_csv ("accumRainfall.csv", sep = '\t', encoding='utf-16')
 
-date_col_name = input ('Please enter the column name for datetime from the table:')
-value_col_name = input ('Please enter the column name for values from the table:')
-running_mins = int (input ('Please enter total running minute:'))
 
-df[date_col_name] = pd.to_datetime(df[date_col_name],unit='s')
+try:
+    date_col_name = input ('Please enter the column name for datetime from the table:')
+    value_col_name = input ('Please enter the column name for values from the table:')
+    running_mins = int (input ('Please enter total running minute:'))
 
-#df['unixdatetime'].index = pd.DatetimeIndex(df['unixdatetime'].index).tz_localize('UTC').tz_convert('US/Eastern')
+    df[date_col_name] = pd.to_datetime(df[date_col_name],unit='s')
+    date_time = df[date_col_name].tolist()
+    value = df[value_col_name].tolist()
+except KeyError:
+    print('Please enter correct column name.')
+    exit()
+except ValueError:
+    print('Please enter valid running time.')
+    exit()
 
-date_time = df[date_col_name].tolist()
-value = df[value_col_name].tolist()
+
 
 def max_rainfall(l) :
 
@@ -30,8 +37,6 @@ def max_rainfall(l) :
     peak_value = max_value[1]
     return max_index, peak_value
 
-
-
 def accumulate(i) :
     total = 0
     for v in i :
@@ -40,5 +45,5 @@ def accumulate(i) :
 
 time_index, peak_rain = max_rainfall(value)
 
-print ('The de-accumulated rain fall data are:', value)
+#print ('The de-accumulated rain fall data are:', value)
 print('Peak period starts after around', int(time_index/len(date_time)*running_mins) ,'mins, and the accumulated rain fall for the peak periold is:', round(peak_rain,2),'mm.')
